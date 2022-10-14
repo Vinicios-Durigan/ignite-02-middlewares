894303df71dc992bf06b49feaa6c3afbe7c2e49d
Vinicios-Durigan <62732923+Vinicios-Durigan@users.noreply.github.com>
2022-10-13 21:25:06 -0400

    # middlewares desafio

--- src/index.js
+++ src/index.js
@@ -10,19 +10,64 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
-}
+  const { username } = request.headers;
+  const user = users.find(user => user.username === username);
 
+  if (!user) {
+    return response.status(404).json({error: "User not Exists"})  
+  }   
+  request.user = user;
+  return next();
+}
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+  const  { user } = request;
+  if (user.pro === true) { 
+    return next();
+  }
+
+  if (user.pro === false && user.todos.length >= 10) {
+    return response.status(403).json({ error: "User is not on the paid plan" })
+  } else { 
+     return next();
+  }
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+  const { id } = request.params;
+  
+  const user = users.find(user => user.username === username);
+
+  if (!user) { 
+     return response.status(404).json()
+  }
+
+  if (!validate(id)) { 
+    return response.status(400).json(); 
+  }
+
+  if (!user.todos[0]) { 
+    return response.status(404).json(); 
+  }
+  
+  request.user = user;
+  request.todo = user.todos[0];
+    
+  return next();
+
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  const { id } = request.params;
+  
+  const user = users.find(user => user.id === id);
+
+  if (!user) { 
+    return response.status(404).json({ error: "User not Exists" }); 
+  }
+
+  request.user = user;
+  return next();
 }
 
 app.post('/users', (request, response) => {
